93b6619b6b22151cbb7b878a8ccb63a124d2ea98
p3arl <p3arl94@protonmail.com>
2024-02-21 18:32:54 +0100

    defined class for snake body

--- snake.py
+++ snake.py
@@ -23,12 +23,29 @@ BODY_SIZE=2
 SPEED = 200
 
 #COLOURS:
-SNAKECOLOR='#00FF00'
-FOODCOLOR='#FFFFFF'
-BACKGROUNDCOLOR='#000000'
+SNAKE_COLOR='#00FF00'
+FOOD_COLOR='#FFFFFF'
+BACKGROUND_COLOR='#000000'
 
 ####################
 
 #define class for the snake object:
 
 class snake
+
+#define sole method, initialize
+
+#using methds from tkinter: coordinates, canvas, create_rectangle, squares
+
+    def __init__(self):
+        self.body_size = BODY_SIZE
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_SIZE):
+            self.coordinates.append([0,0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE, tag="snake")
+            self.squares.append(square)
+
