833901d33bb86af79cbee69a36b420c299344f3a
p3arl <p3arl94@protonmail.com>
2024-02-21 20:56:53 +0100

    works

--- snake.py
+++ snake.py
@@ -31,7 +31,7 @@ BACKGROUND_COLOR='#000000'
 
 #define class for the snake object:
 
-class snake:
+class Snake:
 
 #define sole method, initialize
 
@@ -53,7 +53,7 @@ class snake:
 
 #using randint from random to place food somewhere
 
-class food:
+class Food:
 
     def __init__(self):
 
@@ -73,20 +73,20 @@ class food:
 
 def next_turn(snake, food):
 
-    x, y = snake.coordinates[0,0]
+    x, y = snake.coordinates[0]
 
-    if direction=="UP":
+    if direction=="up":
         y -= SPACE_SIZE
     elif direction == "down":
         y += SPACE_SIZE
     elif direction == "left":
         x -= SPACE_SIZE
-    elif direction == "right"
+    elif direction == "right":
         x += SPACE_SIZE
 
     snake.coordinates.insert(0,(x,y))
 
-    square = canvas.create_recantagle(x,y,(x+SPACE_SIZE),(y+SPACE_SIZE), fill = SNAKE_COLOR)
+    square = canvas.create_rectangle(x,y,(x+SPACE_SIZE),(y+SPACE_SIZE), fill = SNAKE_COLOR)
 
     snake.squares.insert(0,square)
 
@@ -95,13 +95,14 @@ def next_turn(snake, food):
         score +=1
         label.config(text='Points{}'.format(score))
         canvas.delete('food')
-        food = food()
+        food = Food()
     else:
         del snake.coordinates[-1]
         canvas.delete(snake.squares[-1])
         del snake.squares[-1]
 
     if check_collisions(snake):
+        print("collision detected... game over")
         game_over()
     else:
         window.after(SPEED, next_turn, snake, food)
@@ -131,14 +132,16 @@ def check_collisions(snake):
 
     x,y = snake.coordinates[0]
 
+    #if snake exceeds the boundaries of its domain
     if x<0 or x>=WIDTH:
         return True
     elif y<0 or y>=HEIGHT:
         return True
 
-    for body_part in snake.coordinates[1:]:
-        if x==body_part[0] and y==body_part[1]:
-            return True
+    #self-interaction
+ #   for body_part in snake.coordinates[1:]:
+ #       if x==body_part[0] and y==body_part[1]:
+ #           return True
 
     return False
 
@@ -151,3 +154,43 @@ def game_over():
 
 #thats the end of function definitions
 #########################
+
+
+#main tk function
+
+window = Tk()
+window.title('Tk Snake(python)')
+
+score = 0
+direction='down'
+
+label = Label(window, text="Points: {}".format(score),font=('consolas,20'))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2)-(window_width/2))
+y = int((screen_height/2)-(window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+window.bind('<Left>',lambda event: change_direction('left'))
+window.bind('<Right>',lambda event: change_direction('right'))
+window.bind('<Up>',lambda event: change_direction('up'))
+window.bind('<Down>',lambda event: change_direction('down'))
+
+#restart
+#window.bind('<Return>',lambda event: restart)
+
+snake = Snake()
+food = Food()
+
+next_turn(snake,food)
+
+window.mainloop()
