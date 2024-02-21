cbc9cbe1b9f26cd5ed85558a2b81e6ee346e94cb
p3arl <p3arl94@protonmail.com>
2024-02-21 18:22:59 +0100

    inititial commit

---
+++ snake.py
@@ -0,0 +1,34 @@
+#
+#python tkinter snake game... 
+#based off web results from quick google search
+#practice and experience with tkinter
+#
+
+#IMPORTS
+from tkinter import *
+import random
+
+######################
+
+#INITIALISATION:
+#(capitalise constants.)
+
+#GAME SCREEN DIMENSIONS:
+WIDTH=500
+HEIGHT=500
+SPACE_SIZE=20
+
+#SNAKE INITIALISATION:
+BODY_SIZE=2
+SPEED = 200
+
+#COLOURS:
+SNAKECOLOR='#00FF00'
+FOODCOLOR='#FFFFFF'
+BACKGROUNDCOLOR='#000000'
+
+####################
+
+#define class for the snake object:
+
+class snake
